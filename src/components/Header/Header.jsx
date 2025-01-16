50c165b199677ec49833d488bb5df8bbf6864182
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-01-15 21:58:24 -0300

    :sparkles: feat: update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,10 @@ import "../../assets/styles/Header.scss";
 export default function Header() {
   return (
     <header>
-      <div className="container">
-        <img src={Logo} alt="Logo de circulo com gradiente rosa e com texto base apparel ao lado" />
-      </div>
+      <img
+        src={Logo}
+        alt="Logo de circulo com gradiente rosa e com texto base apparel ao lado"
+      />
     </header>
-  )
+  );
 }
